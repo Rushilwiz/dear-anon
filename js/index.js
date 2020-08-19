3fa3357c3e4c3063f59fc68076babac91b028a72
rushilwiz <rushilwiz@gmail.com>
2020-08-18 22:54:27 -0400

    setup master

--- js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("#nothing").on("click", function() {
-    setText();
-  });
-});
-
-function setText () {
-  $("#nothing-header").text("don't you have anything better to do?");
-}
